a5bb767f650779b43d50f81f12a8cbbecf021ddf
wangquan@m-wise.nl <wangquan@m-wise.nl>
2023-11-03 18:05:31 +0100

    course 2023-11-03

--- src/com/harry/service/If_Else.java
+++ src/com/harry/service/If_Else.java
@@ -3,14 +3,11 @@ package com.harry.service;
 public class If_Else {
 	public static void main(String args[]) {
       int grade=9;
-      if(grade==10) {
-    	  System.out.println("Perfect");
-      }else if(grade>=9&&grade<10) {
-    	  System.out.println("Execellent");
-      }else if(grade>=6&&grade<9) {
-    	  System.out.println("Sufficient");
+      
+      if(grade<10) {
+    	  System.out.println("Not Perfect");
       }else {
-    	  System.out.println("Insufficient");
+    	  System.out.println("Perfect");
       }
 	}
 }
--- src/com/harry/service/Loop_For.java
+++ src/com/harry/service/Loop_For.java
@@ -5,11 +5,17 @@ public class Loop_For {
      int start=1;
      int end=100;
      int total=0;
-     for(int i=start;i<=end;i++) {
+     
+     for(int i=1;i<=100;i++) {
     	 total=total+i;
      }
      System.out.println(total);
-     //test
-     System.out.println(total);
+     System.out.println(CalculateSum(12,8,30));
+	}
+	
+	
+	//method/function
+	public static int CalculateSum(int p1, int p2, int p3) {
+		return p1+p2;
 	}
 }
